dc22b16e894035b968e447b7d4e3d076d8dbd13f
Taweechai Yuenyang <krumii.it@gmailcom>
2022-09-28 08:45:56 +0700

    update generate invoice

--- controllers/order.go
+++ controllers/order.go
@@ -8,6 +8,8 @@ import (
 
 func GenerateOrder(c *fiber.Ctx) error {
 	var r models.Response
-	services.CreateOrder()
+	go services.CreateOrder()
+	r.Message = "Auto Generate Order"
+	r.Data = nil
 	return c.Status(fiber.StatusCreated).JSON(&r)
 }
