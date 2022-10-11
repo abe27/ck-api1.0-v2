2e64f14acfc05809412b9f74b04870d6f9d69e0d
Parzival Yuenyang <krumii.it@gmail.com>
2022-10-11 16:44:25 +0700

    new order limit

--- controllers/order.go
+++ controllers/order.go
@@ -12,7 +12,7 @@ func GetAllOrder(c *fiber.Ctx) error {
 	var obj []models.Order
 	// Fetch All Data
 	err := configs.Store.
-		Limit(50).
+		Limit(100).
 		Order("etd_date").
 		Preload("Consignee.Whs").
 		Preload("Consignee.Factory").
