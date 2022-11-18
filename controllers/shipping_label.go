f66fb032540ed710522ce7658231a933541a48e6
abe27 <krumii.it@gmail.com>
2022-11-18 09:28:01 +0700

    set print label

--- controllers/shipping_label.go
+++ controllers/shipping_label.go
@@ -47,7 +47,7 @@ func DeleteShippingLabelByID(c *fiber.Ctx) error {
 
 func CreatePrintLabel(c *fiber.Ctx) error {
 	var r models.Response
-	var frm models.PrintShippingLabel
+	var frm []models.PrintShippingLabel
 	err := c.BodyParser(&frm)
 	if err != nil {
 		r.Message = err.Error()
@@ -55,38 +55,44 @@ func CreatePrintLabel(c *fiber.Ctx) error {
 	}
 
 	db := configs.Store
+	var data []models.PrintShippingLabel
+	for _, p := range frm {
+		obj := models.PrintShippingLabel{
+			InvoiceNo:    p.InvoiceNo,
+			OrderNo:      p.OrderNo,
+			PartNo:       p.PartNo,
+			CustCode:     p.CustCode,
+			CustName:     p.CustName,
+			PalletNo:     p.PalletNo,
+			PrintDate:    p.PrintDate,
+			Qty:          p.Qty,
+			QrCode:       fmt.Sprintf("06P%s;17Q%d;30T%s;32T%s;", p.PartNo, p.Qty, p.OrderNo, p.BarCode),
+			BarCode:      p.BarCode,
+			LabelBarCode: fmt.Sprintf("*%s*", p.BarCode),
+			IsPrint:      p.IsPrint,
+		}
+		err = db.FirstOrCreate(&obj, &models.PrintShippingLabel{BarCode: p.BarCode}).Error
+		if err != nil {
+			r.Message = err.Error()
+			return c.Status(fiber.StatusInternalServerError).JSON(&r)
+		}
 
-	obj := models.PrintShippingLabel{
-		InvoiceNo:    frm.InvoiceNo,
-		OrderNo:      frm.OrderNo,
-		PartNo:       frm.PartNo,
-		CustCode:     frm.CustCode,
-		CustName:     frm.CustName,
-		PalletNo:     frm.PalletNo,
-		PrintDate:    frm.PrintDate,
-		Qty:          frm.Qty,
-		QrCode:       fmt.Sprintf("06P%s;17Q%d;30T%s;32T%s;", frm.PartNo, frm.Qty, frm.OrderNo, frm.BarCode),
-		BarCode:      frm.BarCode,
-		LabelBarCode: fmt.Sprintf("*%s*", frm.BarCode),
-		IsPrint:      frm.IsPrint,
+		obj.Qty = p.Qty
+		obj.QrCode = fmt.Sprintf("06P%s;17Q%d;30T%s;32T%s;", p.PartNo, p.Qty, p.OrderNo, p.BarCode)
+		obj.IsPrint = 0
+		err = db.Save(&obj).Error
+		if err != nil {
+			r.Message = err.Error()
+			return c.Status(fiber.StatusInternalServerError).JSON(&r)
+		}
+		data = append(data, obj)
 	}
 
-	err = db.FirstOrCreate(&obj, &models.PrintShippingLabel{BarCode: frm.BarCode}).Error
-	if err != nil {
-		r.Message = err.Error()
-		return c.Status(fiber.StatusInternalServerError).JSON(&r)
+	txt := fmt.Sprintf("%d รายการ", len(data))
+	if len(data) == 1 {
+		txt = data[0].BarCode
 	}
-
-	obj.Qty = frm.Qty
-	obj.QrCode = fmt.Sprintf("06P%s;17Q%d;30T%s;32T%s;", frm.PartNo, frm.Qty, frm.OrderNo, frm.BarCode)
-	obj.IsPrint = 0
-	err = db.Save(&obj).Error
-	if err != nil {
-		r.Message = err.Error()
-		return c.Status(fiber.StatusInternalServerError).JSON(&r)
-	}
-
-	r.Message = services.MessagePrintShippingLabel(frm.BarCode)
-	r.Data = &obj
+	r.Message = services.MessagePrintShippingLabel(txt)
+	r.Data = &data
 	return c.Status(fiber.StatusOK).JSON(&r)
 }
