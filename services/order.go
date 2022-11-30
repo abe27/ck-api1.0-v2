0b54ecfad004b7496d5928f872e431ad1851ff70
Taweechai Yuenyang <krumii.it@gmail.com>
2022-11-30 10:55:44 +0700

    update revise

--- services/order.go
+++ services/order.go
@@ -11,6 +11,20 @@ import (
 )
 
 func CreateOrder(factory, end_etd string) {
+	var orderTitle models.OrderTitle
+	if err := configs.Store.Select("id,title").Where("title=?", "000").First(&orderTitle).Error; err != nil {
+		panic(err)
+	}
+	CreateOrderWithOutRevise(factory, end_etd, &orderTitle)
+	/// After Generate Order Get Tap Data
+	GenerateImportInvoiceTap()
+	// After Get Tap
+	CreateOrderWithRevise(factory, end_etd, &orderTitle)
+	/// After Generate Order Get Tap Data
+	GenerateImportInvoiceTap()
+}
+
+func CreateOrderWithOutRevise(factory, endDate string, orderTitle *models.OrderTitle) {
 	// Group Order to create order ent
 	db := configs.Store
 	var ord []models.OrderPlan
@@ -21,7 +35,7 @@ func CreateOrder(factory, end_etd string) {
 		Where("is_generate=?", false).
 		Where("is_revise_error=?", false).
 		Where("vendor=?", &factory).
-		Where("etd_tap <= ?", end_etd).
+		Where("etd_tap <= ?", endDate).
 		Group("order_zone_id,consignee_id,shipment_id,etd_tap,pc_id,commercial_id,bioabt,order_group,vendor,biac,bishpc,bisafn,sample_flg_id,carrier_code").
 		Find(&ord).Error; err != nil {
 		sysLogger := models.SyncLogger{
@@ -29,11 +43,6 @@ func CreateOrder(factory, end_etd string) {
 			Description: fmt.Sprintf("Error fetch order: %v", err),
 		}
 		db.Create(&sysLogger)
-		panic(err)
-	}
-
-	var orderTitle models.OrderTitle
-	if err := db.Select("id,title").Where("title=?", "000").First(&orderTitle).Error; err != nil {
 		panic(err)
 	}
 
@@ -166,15 +175,12 @@ func CreateOrder(factory, end_etd string) {
 			}
 		}
 	}
-
-	// CreateOrderWithRevise(factory, end_etd, &orderTitle)
-	// GenerateImportInvoiceTap()
 }
 
 func CreateOrderWithRevise(factory, endDate string, orderTitle *models.OrderTitle) {
 	db := configs.Store
-	var ord []models.OrderPlan
-	if err := db.Raw("select * from tbt_order_plans where length(reasoncd) > 0 and is_generate=false and is_revise_error=false and vendor=? order by upddte,updtime,seq", factory).Scan(&ord).Error; err != nil {
+	var ordDetail []models.OrderPlan
+	if err := db.Raw("select * from tbt_order_plans where length(reasoncd) > 0 and is_generate=false and is_revise_error=false and vendor=? order by upddte,updtime,seq", factory).Scan(&ordDetail).Error; err != nil {
 		sysLogger := models.SyncLogger{
 			Title:       "generate order ent revises",
 			Description: fmt.Sprintf("%v", err),
@@ -184,50 +190,54 @@ func CreateOrderWithRevise(factory, endDate string, orderTitle *models.OrderTitl
 	}
 	/// parse time
 	parseEndDate, _ := time.Parse("2006-01-02", endDate)
-	i := 0
-	for i < len(ord) {
+	for _, ord := range ordDetail {
 		// GenerateOrderDetailWithRevise(end_etd, &ord[i], orderTitle)
-		if !(ord[i].EtdTap.After(parseEndDate)) {
-			fmt.Printf("ID: %s ETD: %s Revise: %s IsAfter: %v\n", ord[i].ID, ord[i].EtdTap.Format("2006-01-02"), ord[i].Reasoncd[:1], (ord[i].EtdTap.After(parseEndDate)))
+		if !(ord.EtdTap.After(parseEndDate)) {
+			fmt.Printf("ID: %s ETD: %s Revise: %s IsAfter: %v\n", ord.ID, ord.EtdTap.Format("2006-01-02"), ord.Reasoncd[:1], (ord.EtdTap.After(parseEndDate)))
 			var order models.Order
 			db.Order("created_at desc").Select("id").Last(&order, &models.Order{
-				ConsigneeID:  ord[i].ConsigneeID,
-				EtdDate:      &ord[i].EtdTap,
-				ShipmentID:   ord[i].ShipmentID,
-				PcID:         ord[i].PcID,
-				CommercialID: ord[i].CommercialID,
-				SampleFlgID:  ord[i].SampleFlgID,
-				Bioabt:       ord[i].Bioabt,
+				ConsigneeID:  ord.ConsigneeID,
+				EtdDate:      &ord.EtdTap,
+				ShipmentID:   ord.ShipmentID,
+				PcID:         ord.PcID,
+				CommercialID: ord.CommercialID,
+				SampleFlgID:  ord.SampleFlgID,
+				Bioabt:       ord.Bioabt,
 			})
 
 			if order.ID == "" {
-				etd := ord[i].EtdTap.Format("20060102")
+				etd := ord.EtdTap.Format("20060102")
 				var ship models.Shipment
-				db.Select("id,title").First(&ship, "id=?", ord[i].ShipmentID)
+				if err := db.Select("id,title").First(&ship, "id=?", ord.ShipmentID).Error; err != nil {
+					panic(err)
+				}
+
+				fmt.Printf("Shipment ID: %s\n", ship.ID)
 				/// Generate ZoneCode
 				var ordCount int64
-				db.Select("id").Where("etd_date=?", ord[i].EtdTap).Find(&models.Order{}).Count(&ordCount)
-				sum := ordCount + 1
-				keyCode := fmt.Sprintf("%s%s%03d", etd[2:], ship.Title, sum)
-				var sumOrder models.Order
-				db.Select("id").First(&sumOrder, "zone_code=?", keyCode)
-				for !(len(sumOrder.ID) == 0) {
-					keyCode = fmt.Sprintf("%s%s%03d", etd[2:], ship.Title, sum)
+				if err := db.Select("id").Where("etd_date=?", ord.EtdTap).Find(&models.Order{}).Count(&ordCount).Error; err != nil {
+					panic(err)
 				}
+
+				fmt.Printf("Count Order: %d\n", ordCount)
+				keyCode := fmt.Sprintf("%s%s", etd[2:], ship.Title)
+				var countSeq int64
+				db.Raw("SELECT count(id) + 1 FROM tbt_orders WHERE zone_code like ?", keyCode+"%").Scan(&countSeq)
+				keyCode = fmt.Sprintf("%s%s%03d", etd[2:], ship.Title, countSeq)
 
 				// Check LoadingArea
 				// fmt.Printf("Check LoadingArea %s\n", ord[x].OrderGroup[:1])
 				prefixOrder := "-"
-				if ord[i].OrderGroup[:1] == "@" {
+				if ord.OrderGroup[:1] == "@" {
 					prefixOrder = "@"
 				}
 				var loadingData models.OrderLoadingArea
-				db.Select("prefix,loading_area,privilege").Where("order_zone_id=?", ord[i].OrderZoneID).Where("prefix=?", prefixOrder).First(&loadingData)
+				db.Select("prefix,loading_area,privilege").Where("order_zone_id=?", ord.OrderZoneID).Where("prefix=?", prefixOrder).First(&loadingData)
 
 				var factoryEnt models.Factory
-				db.Select("id,title").Where("title=?", ord[i].Vendor).First(&factoryEnt)
+				db.Select("id,title").Where("title=?", ord.Vendor).First(&factoryEnt)
 				var affcodeData models.Affcode
-				db.Select("id,title,description").Where("title=?", ord[i].Biac).First(&affcodeData)
+				db.Select("id,title,description").Where("title=?", ord.Biac).First(&affcodeData)
 
 				// Get LastInvoiceNo
 				invSeq := models.LastInvoice{
@@ -244,21 +254,21 @@ func CreateOrderWithRevise(factory, endDate string, orderTitle *models.OrderTitl
 				})
 
 				// Fetch Order Entries
-				order.ConsigneeID = ord[i].ConsigneeID
-				order.ShipmentID = ord[i].ShipmentID
-				order.EtdDate = &ord[i].EtdTap
-				order.PcID = ord[i].PcID
-				order.CommercialID = ord[i].CommercialID
-				order.SampleFlgID = ord[i].SampleFlgID
+				order.ConsigneeID = ord.ConsigneeID
+				order.ShipmentID = ord.ShipmentID
+				order.EtdDate = &ord.EtdTap
+				order.PcID = ord.PcID
+				order.CommercialID = ord.CommercialID
+				order.SampleFlgID = ord.SampleFlgID
 				order.OrderTitleID = &orderTitle.ID
-				order.Bioabt = ord[i].Bioabt
+				order.Bioabt = ord.Bioabt
 				order.ZoneCode = keyCode
 				order.LoadingArea = loadingData.LoadingArea
 				order.Privilege = loadingData.Privilege
-				order.ShipForm = ord[i].Bishpc         // bishpc
-				order.ShipTo = ord[i].Bisafn           // bisafn
-				order.SampleFlg = ord[i].SampleFlg     // sample_flg
-				order.CarrierCode = ord[i].CarrierCode // carriercod
+				order.ShipForm = ord.Bishpc         // bishpc
+				order.ShipTo = ord.Bisafn           // bisafn
+				order.SampleFlg = ord.SampleFlg     // sample_flg
+				order.CarrierCode = ord.CarrierCode // carriercod
 				order.RunningSeq = (invSeq.LastRunning + 1)
 				order.IsActive = false
 				order.IsSync = true
@@ -271,12 +281,12 @@ func CreateOrderWithRevise(factory, endDate string, orderTitle *models.OrderTitl
 
 			if order.ID != "" {
 				fmt.Printf("ID: %s\n", order.ID)
-				CreateOrderDetail(&order, &ord[i])
+				CreateOrderDetail(&order, &ord)
 			}
 		} else {
 			var orderDetail models.OrderDetail
-			if err := db.Where("pono=?", &ord[i].Pono).Where("ledger_id=?", ord[i].LedgerID).Where("total_on_pallet=?", 0).First(&orderDetail).Error; err == nil {
-				fmt.Printf("Order %s found ID: %s  OrderID: %s ETD: %s Revise: %s\n", ord[i].ID, orderDetail.ID, *orderDetail.OrderID, ord[i].EtdTap, ord[i].Reasoncd)
+			if err := db.Where("pono=?", &ord.Pono).Where("ledger_id=?", ord.LedgerID).Where("total_on_pallet=?", 0).First(&orderDetail).Error; err == nil {
+				fmt.Printf("Order %s found ID: %s  OrderID: %s ETD: %s Revise: %s\n", ord.ID, orderDetail.ID, *orderDetail.OrderID, ord.EtdTap, ord.Reasoncd)
 				if err := db.Delete(&models.OrderDetail{}, "id", orderDetail.ID).Error; err == nil {
 					var countOrdDetail int64
 					db.Select("id").Where("order_id=?", orderDetail.OrderID).Find(&models.OrderDetail{}).Count(&countOrdDetail)
@@ -288,8 +298,7 @@ func CreateOrderWithRevise(factory, endDate string, orderTitle *models.OrderTitl
 				}
 			}
 		}
-		DeleteOrder(&ord[i])
-		i++
+		DeleteOrder(&ord)
 	}
 }
 
