c6a7882fc3768c0c03d87e01fa7d8874270194d5
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-06 14:31:44 +0700

    show url

--- controllers/stock.go
+++ controllers/stock.go
@@ -70,7 +70,52 @@ func CreateStock(c *fiber.Ctx) error {
 }
 
 func ShowStockByID(c *fiber.Ctx) error {
+	db := configs.Store
 	var r models.Response
+	method := "GET"
+	tag := "C"
+	if c.Query("tag") != "" {
+		tag = c.Query("tag")
+	}
+
+	url := fmt.Sprintf("%s/stock/%s?tag=%s", configs.API_TRIGGER_URL, c.Params("id"), tag)
+	// fmt.Println(url)
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		panic(err)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var obj models.OraResponse
+	if err = json.Unmarshal(body, &obj); err != nil {
+		panic(err)
+	}
+
+	// Get PartName
+	var data []models.OraStock
+	for _, i := range obj.Data {
+		var p models.Part
+		if err = db.Where("title=?", i.PartNo).First(&p).Error; err != nil {
+			panic(err)
+		}
+		i.Slug = p.Slug
+		i.PartName = p.Description
+		data = append(data, i)
+	}
+
+	r.Message = obj.Message
+	r.Data = &data
 	return c.Status(fiber.StatusOK).JSON(&r)
 }
 
