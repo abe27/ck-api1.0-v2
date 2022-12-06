bbc456e9987da6606001454a3695d5f13cf8b4f7
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-06 16:48:16 +0700

    update stock oracle

--- models/ora.go
+++ models/ora.go
@@ -11,19 +11,19 @@ type OraResponse struct {
 }
 
 type OraStock struct {
-	ID        int64     `json:"id,omitempty"`
-	Tagrp     string    `json:"tagrp,omitempty"`
-	Slug      string    `json:"slug,omitempty"`
-	PartNo    string    `json:"part_no,omitempty"`
-	PartName  string    `json:"part_name,omitempty"`
-	SerialNo  string    `json:"serial_no,omitempty"`
-	LotNo     string    `json:"lot_no,omitempty"`
-	LineNo    string    `json:"die_no,omitempty"`
-	ReviseNo  string    `json:"revise_no,omitempty"`
-	Shelve    string    `json:"shelve,omitempty"`
-	PalletNo  string    `json:"pallet_no,omitempty"`
-	Qty       float64   `json:"qty,omitempty"`
-	Ctn       float64   `json:"ctn,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	ID        int64     `json:"id"`
+	Tagrp     string    `json:"tagrp"`
+	Slug      string    `json:"slug"`
+	PartNo    string    `json:"part_no"`
+	PartName  string    `json:"part_name"`
+	SerialNo  string    `json:"serial_no"`
+	LotNo     string    `json:"lot_no"`
+	LineNo    string    `json:"die_no"`
+	ReviseNo  string    `json:"revise_no"`
+	Shelve    string    `json:"shelve"`
+	PalletNo  string    `json:"pallet_no"`
+	Qty       float64   `json:"qty"`
+	Ctn       float64   `json:"ctn"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
