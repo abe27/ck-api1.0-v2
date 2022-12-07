7a52612004b056f9e362bb9d5b30284bef220a69
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-07 15:02:08 +0700

    update show receive

--- controllers/receive_ent.go
+++ controllers/receive_ent.go
@@ -56,6 +56,30 @@ func CreateReceiveEnt(c *fiber.Ctx) error {
 
 func ShowReceiveEntByID(c *fiber.Ctx) error {
 	var r models.Response
+	var obj models.Receive
+	db := configs.Store
+	err := db.
+		Limit(25).
+		Order("receive_date desc,transfer_out_no").
+		Preload("FileEdi.Factory").
+		Preload("FileEdi.Mailbox.Area").
+		Preload("FileEdi.FileType").
+		Preload("ReceiveType.Whs").
+		Preload("ReceiveDetail.Ledger.Part").
+		Preload("ReceiveDetail.Ledger.PartType").
+		Preload("ReceiveDetail.Ledger.Unit").
+		Preload("ReceiveDetail.CartonNotReceive").
+		Where("id=?", c.Params("id")).
+		First(&obj).
+		Error
+	if err != nil {
+		r.Message = services.MessageSystemError
+		r.Data = &r
+		return c.Status(fiber.StatusInternalServerError).JSON(&r)
+	}
+
+	r.Message = services.MessageShowAllData("receive")
+	r.Data = &obj
 	return c.Status(fiber.StatusOK).JSON(&r)
 }
 
