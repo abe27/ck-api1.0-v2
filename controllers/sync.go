8263cd1bc5e9e0ea181f00896dffa6327cd2609d
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-15 10:40:51 +0700

    update show order by limit

--- controllers/sync.go
+++ controllers/sync.go
@@ -59,7 +59,7 @@ func GetSyncOrderList(c *fiber.Ctx) error {
 	var r models.Response
 	var obj []models.Order
 	if err := configs.Store.
-		Limit(5).
+		Limit(20).
 		Order("etd_date,updated_at").
 		Where("is_checked=?", true).
 		Where("is_sync=?", false).
