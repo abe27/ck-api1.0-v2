a4b4a170d51f49207e56a482f67ad61ba23ed269
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-19 13:08:01 +0700

    new limit

--- controllers/invoice_tap.go
+++ controllers/invoice_tap.go
@@ -27,7 +27,7 @@ func ImportInvoiceTap(c *fiber.Ctx) error {
 	}
 
 	//// Read Excel
-	go services.ImportInvoiceTap(&fName)
+	services.ImportInvoiceTap(&fName)
 	r.Message = services.MessageUploadFileCompleted(fName)
 	return c.Status(fiber.StatusCreated).JSON(&r)
 }
