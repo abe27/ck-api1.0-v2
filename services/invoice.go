fe56e047ae53309790726adfac4750a2846a7ae9
Taweechai Yuenyang <krumii.it@gmail.com>
2022-12-22 14:55:46 +0700

    update generate

--- services/invoice.go
+++ services/invoice.go
@@ -68,6 +68,95 @@ func ImportInvoiceTap(fileName *string) {
 					bhcbmt, _ := r.GetCol(19)
 					// fmt.Printf("Start inv: %s\n", bhivno.GetString())
 					if bhivno.GetString() != "" {
+						d := bhivdt.GetString()
+						if len(d) <= 5 {
+							d = fmt.Sprintf("0%s", bhivdt.GetString())
+						}
+						dd := fmt.Sprintf("20%s%s%s", d[4:6], d[2:4], d[:2])
+						etd, _ := time.Parse("20060102", dd)
+						Bhcon, _ := strconv.ParseInt(bhcon.GetString(), 10, 64)
+						Bhctn, _ := strconv.ParseInt(bhctn.GetString(), 10, 64)
+						Bhwidt, _ := strconv.ParseInt(bhwidt.GetString(), 10, 64)
+						Bhleng, _ := strconv.ParseInt(bhleng.GetString(), 10, 64)
+						Bhhigh, _ := strconv.ParseInt(bhhigh.GetString(), 10, 64)
+						Bhgrwt, _ := strconv.ParseFloat(bhgrwt.GetString(), 64)
+						Bhcbmt, _ := strconv.ParseFloat(bhcbmt.GetString(), 64)
+						var invTap models.ImportInvoiceTap
+						invTap.Biseq = int64(line)
+						invTap.Bhivno = bhivno.GetString()
+						invTap.Bhodpo = bhodpo.GetString()
+						invTap.BhodpoTrim = strings.Trim(strings.ReplaceAll(bhodpo.GetString(), " ", ""), "")
+						invTap.Bhivdt = etd
+						invTap.Bhconn = bhconn.GetString()
+						invTap.Bhcons = bhcons.GetString()
+						invTap.Bhsven = bhsven.GetString()
+						invTap.Bhshpf = bhshpf.GetString()
+						invTap.Bhsafn = bhsafn.GetString()
+						invTap.Bhshpt = bhshpt.GetString()
+						invTap.Bhfrtn = bhfrtn.GetString()
+						invTap.Bhcon = Bhcon
+						invTap.Bhpaln = bhpaln.GetString()
+						invTap.Bhpnam = bhpnam.GetString()
+						invTap.Bhypat = bhypat.GetString()
+						invTap.Bhctn = Bhctn
+						invTap.Bhwidt = Bhwidt
+						invTap.Bhleng = Bhleng
+						invTap.Bhhigh = Bhhigh
+						invTap.Bhgrwt = Bhgrwt
+						invTap.Bhcbmt = Bhcbmt
+						invTap.IsMatched = false
+						db.FirstOrCreate(&invTap, &models.ImportInvoiceTap{
+							Bhivno:     bhivno.GetString(),
+							Bhodpo:     bhodpo.GetString(),
+							BhodpoTrim: strings.Trim(strings.ReplaceAll(bhodpo.GetString(), " ", ""), ""),
+							Bhivdt:     etd,
+							Bhsafn:     bhsafn.GetString(),
+							Bhypat:     bhypat.GetString(),
+							Bhctn:      Bhctn,
+						})
+					}
+				}
+			}
+			line++
+		}
+	}
+}
+
+func FetchInvoiceTap(fileName *string) {
+	workbook, err := xls.OpenFile(*fileName)
+	if err != nil {
+		return
+	}
+
+	for _, sheet := range workbook.GetSheets() {
+		// // sname := sheet.GetName()
+		db := configs.Store
+		line := 1
+		for i := 0; i <= sheet.GetNumberRows(); i++ {
+			if i > 0 {
+				if r, err := sheet.GetRow(i); err == nil {
+					bhivno, _ := r.GetCol(0)
+					bhodpo, _ := r.GetCol(1)
+					bhivdt, _ := r.GetCol(2)
+					bhconn, _ := r.GetCol(3)
+					bhcons, _ := r.GetCol(4)
+					bhsven, _ := r.GetCol(5)
+					bhshpf, _ := r.GetCol(6)
+					bhsafn, _ := r.GetCol(7)
+					bhshpt, _ := r.GetCol(8)
+					bhfrtn, _ := r.GetCol(9)
+					bhcon, _ := r.GetCol(10)
+					bhpaln, _ := r.GetCol(11)
+					bhpnam, _ := r.GetCol(12)
+					bhypat, _ := r.GetCol(13)
+					bhctn, _ := r.GetCol(14)
+					bhwidt, _ := r.GetCol(15)
+					bhleng, _ := r.GetCol(16)
+					bhhigh, _ := r.GetCol(17)
+					bhgrwt, _ := r.GetCol(18)
+					bhcbmt, _ := r.GetCol(19)
+					// fmt.Printf("Start inv: %s\n", bhivno.GetString())
+					if bhivno.GetString() != "" {
 						inv := bhivno.GetString()
 						inv_seq, _ := strconv.ParseInt(inv[5:len(inv)-1], 10, 64)
 						var facData models.Factory
